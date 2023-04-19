56d97b138a63b648a5348152c189eabbe51177cb
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-04-19 15:31:48 +0500

    payment method choice

--- apps/order/choices.py
+++ apps/order/choices.py
@@ -7,3 +7,16 @@ class OrderStatusChoice(TextChoices):
     SHIPPED = "S", "Shipped"
     DELIVERED = "D", "Delivered"
     CANCELLED = "X", "Cancelled"
+
+
+class PaymentMethodChoice(TextChoices):
+    CREDIT_CARD = 'CC', 'Credit Card'
+    DEBIT_CARD = 'DC', 'Debit Card'
+    PAYPAL = 'PP', 'PayPal'
+    GOOGLE_PAY = 'GP', 'Google Pay'
+    APPLE_PAY = 'AP', 'Apple Pay'
+    BANK_TRANSFER = 'BT', 'Bank Transfer'
+    CASH_ON_DELIVERY = 'COD', 'Cash on Delivery'
+    BITCOIN = 'BTC', 'Bitcoin'
+    ETHEREUM = 'ETH', 'Ethereum'
+    CASH = 'CA', 'Cash'
--- apps/order/models.py
+++ apps/order/models.py
@@ -1,24 +1,21 @@
-from django.db.models import (CASCADE, CharField, DateTimeField, DecimalField,
-                              ForeignKey, Model, OneToOneField,
-                              PositiveIntegerField)
+from django.db.models import (CASCADE, CharField, DecimalField, ForeignKey,
+                              OneToOneField, PositiveIntegerField)
 
 from apps.common.models import TimeStampedModel
-from apps.order.choices import OrderStatusChoice
+from apps.order.choices import OrderStatusChoice, PaymentMethodChoice
 
 
 class Cart(TimeStampedModel):
-    user = ForeignKey("users.User", CASCADE, "carts")
-    product = ForeignKey("product.Product", CASCADE, "cart")
+    user = ForeignKey("users.User", CASCADE, "carts", verbose_name='User')
 
     class Meta:
-        unique_together = ["user", "product"]
         db_table = "cart"
         verbose_name_plural = "Carts"
 
 
 class CartItem(TimeStampedModel):
-    cart = ForeignKey("order.Cart", CASCADE, "cart_items")
-    product = ForeignKey("product.Product", CASCADE, "cart_item")
+    cart = ForeignKey("order.Cart", CASCADE, "cart_items", verbose_name='Cart')
+    product = ForeignKey("product.Product", CASCADE, "cart_item", verbose_name='Product')
     quantity = PositiveIntegerField(default=1, verbose_name="Quantity")
 
     def __str__(self):
@@ -36,6 +33,7 @@ class CartItem(TimeStampedModel):
             self.save()
 
     class Meta:
+        unique_together = ["cart", "product"]
         db_table = "cart_item"
         verbose_name = "CartItem"
         verbose_name_plural = "CartItems"
@@ -47,32 +45,34 @@ class CartItem(TimeStampedModel):
             cart_item.decrement_quantity()
 
 
-class Payment(Model):
-    order = ForeignKey("order.Order", CASCADE)
-    amount = DecimalField(max_digits=10, decimal_places=2)
-    created_at = DateTimeField(auto_now_add=True)
+class Payment(TimeStampedModel):
+    order = ForeignKey("order.Order", CASCADE, verbose_name='Order')
+    amount = DecimalField(max_digits=10, decimal_places=2, verbose_name='Amount')
 
     class Meta:
         db_table = "payment"
 
 
 class Order(TimeStampedModel):
-    user = ForeignKey("users.User", CASCADE)
-    cart = OneToOneField("order.Cart", CASCADE)
-    status = CharField(choices=OrderStatusChoice.choices, default=OrderStatusChoice.PENDING)
+    user = ForeignKey("users.User", CASCADE, verbose_name='Order')
+    payment_method = CharField(max_length=24, choices=PaymentMethodChoice.choices, verbose_name='Payment Method')
+    cart = OneToOneField("order.Cart", CASCADE, verbose_name='Cart')
+    status = CharField(choices=OrderStatusChoice.choices, default=OrderStatusChoice.PENDING, verbose_name='Status')
+    shipping_cost = DecimalField(max_digits=10, decimal_places=2)
+    total = DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         db_table = "order"
 
 
 class ShippingAddress(TimeStampedModel):
-    user = ForeignKey("users.User", CASCADE)
-    order = OneToOneField("order.Order", CASCADE)
-    name = CharField(max_length=255)
-    address = CharField(max_length=255)
-    city = CharField(max_length=255)
-    state = CharField(max_length=255)
-    zip_code = CharField(max_length=10)
+    user = ForeignKey("users.User", CASCADE, verbose_name='User')
+    order = OneToOneField("order.Order", CASCADE, verbose_name='Order')
+    name = CharField(max_length=255, verbose_name='Name')
+    address = CharField(max_length=255, verbose_name='Address')
+    city = CharField(max_length=255, verbose_name='City')
+    state = CharField(max_length=255, verbose_name='State')
+    zip_code = CharField(max_length=10, verbose_name='Zip Code')
 
     class Meta:
         db_table = "shipping"
